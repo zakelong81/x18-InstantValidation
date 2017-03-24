66e3321239805c73b63efe669230689d9e6798cc
Unknown <lnguyen34@mail.csuchico.edu>
2017-03-23 19:10:16 -0700

    upgrade javascript

--- form-validation.js
+++ form-validation.js
@@ -1,12 +1,9 @@
-function validate()
-{
+function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
-
 //username
-  if (userEntered.length >= 6)
-  {
+  if (userEntered.length >= 6)  {
 
     //Turn the username items green
     document.getElementById("usernameGroup").classList.remove("has-error");
@@ -20,9 +17,7 @@ function validate()
     document.getElementById("usernameError").classList.add("shown-message");
 
   }
-
-  else if (userEntered.indexOf(' ') >= 0 )
-  {
+  else if (userEntered.indexOf(' ') >= 0 )  {
     //Show message that there is an error with the username...
     document.getElementById("usernameError").innerHTML="username can not contain spacce";
     document.getElementById("usernameError").classList.remove("hidden-message");
@@ -31,9 +26,7 @@ function validate()
     document.getElementById("usernameGroup").classList.add("has-error");
 
   }
-
-  else
-  {
+  else  {
     //Show message that there is an error with the username...
     document.getElementById("usernameError").innerHTML="User Name must be at least 6 characters";
     document.getElementById("usernameError").classList.remove("hidden-message");
@@ -41,8 +34,118 @@ function validate()
     //Turn the username items red
     document.getElementById("usernameGroup").classList.add("has-error");
   }
-
 //password
+  if (passEntered.toLowerCase() == "password")  {
+    //Show message that there is an error with the password...
+    document.getElementById("passwordError").innerHTML='Password must NOT be the word "password" regardless of (upper-/lower-) case used';
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else if(passEntered.length < 6 || passEntered.length > 20 )  {
+    //Show message that there is an error with the password...
+    document.getElementById("passwordError").innerHTML="Password must be between 6-20 characters";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else if(passEntered == userEntered )  {
+    //Show message that there is an error with the password...
+    document.getElementById("passwordError").innerHTML="Password must NOT be the same as the Username";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else if( passEntered != passEntered.toLowerCase() )  {
+    //Show message that there is an error with the password...
+    document.getElementById("passwordError").innerHTML="Password must contain a upper case";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else if( /\d/.test(passEntered) == false )  {
+    //Show message that there is an error with the password...
+    document.getElementById("passwordError").innerHTML="Password must contain a number";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else if (/[!@#$%^&*()]/g.test(passEntered) == false){
+    //Show message that there is an error with the password...
+    document.getElementById("passwordError").innerHTML="Password must contain a special character such as !@#$%^&*()";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");}
+  else{
+    //Turn the password items green
+    document.getElementById("passwordGroup").classList.remove("has-error");
+    document.getElementById("passwordGroup").classList.add("has-success");
+
+    document.getElementById("passwordError").classList.add("hidden-message");
+    document.getElementById("passwordError").classList.remove("shown-message");
+
+    document.getElementById("passwordError").innerHTML="Success creating password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+  }
+}
+
+function register(){
+  //Grab the user's input and store in variables
+  var userEntered = document.getElementById("user").value;
+  var passEntered = document.getElementById("pass").value;
+  alert("Username is: " + userEntered);
+  document.write("\n");
+  alert("Password is: " + passEntered);
+  document.write("\n");
+}
+function validateUsername(){
+  var userEntered = document.getElementById("user").value;
+  if (userEntered.length >= 6)
+  {
+    //Turn the username items green
+    document.getElementById("usernameGroup").classList.remove("has-error");
+    document.getElementById("usernameGroup").classList.add("has-success");
+
+    document.getElementById("usernameError").classList.add("hidden-message");
+    document.getElementById("usernameError").classList.remove("shown-message");
+
+    document.getElementById("usernameError").innerHTML="Success creating User Name.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+  }
+
+  if (userEntered.length < 6 )
+  {
+    //Show message that there is an error with the username...
+    document.getElementById("usernameError").innerHTML="username must be at least 6 characters";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
+  if (userEntered.indexOf(' ') >= 0 )
+  {
+    //Show message that there is an error with the username...
+    document.getElementById("usernameError").innerHTML="username can not contain spacce";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
+
+}
+
+function validatePassword(){
+  var userEntered = document.getElementById("user").value;
+  var passEntered = document.getElementById("pass").value;
+
   if (passEntered.toLowerCase() == "password")
   {
     //Show message that there is an error with the password...
@@ -116,4 +219,5 @@ function validate()
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
   }
+
 }
